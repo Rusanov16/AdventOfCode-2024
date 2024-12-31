6712abe845d6fea92e3f180075dbb254320565f1
russanov <carlosrusaisidro@gmail.com>
2024-12-30 20:56:22 -0400

    Se agregó la parte dos

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,8 +1,9 @@
 #Importar las ubicaciones
 import pandas as pd
+#Parte Uno
 
 # Crear el dataframe con los datos
-datos = pd.read_csv("C:/Users/hpser/Downloads/adventofcode_2024/day_1/ubi.csv", header= None, delim_whitespace= True)
+datos = pd.read_csv("C:/Users/hpser/Downloads/proyectos/advent_of_code-2024/day_1/ubi.csv", header= None, delim_whitespace= True)
 print(datos)
 print(datos.head())  
 print()
@@ -23,3 +24,15 @@ print(datos)
 #Calculo suma total de la distancia
 total_distancia = sum(datos['Distancia'])
 print(f"La distancia total entre las columnas 'UbiID_1', 'UbiID_2' será de {total_distancia} ")
+
+#Parte Dos
+
+# Calcular la frecuencia en la que cada número de la lista de la izquierda aparece en la lista de la derecha.
+lista_izq = datos['UbiID_1']
+lista_der = datos['UbiID_2']
+
+frecuencia = lista_der.value_counts()
+print(frecuencia)
+
+similitud = (lista_izq.map(frecuencia).fillna(0) * lista_izq).sum()
+print(f"La puntuación de similitud es: {similitud}")
